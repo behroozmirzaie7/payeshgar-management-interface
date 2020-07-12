549770158be76fd7a28ce0963d8cc1c6e8d71c8c
Mahdi Zareie <mahdi.zareie@pm.me>
2020-07-13 02:10:19 +0430

    Design new domain model based on the new architecture

--- monitoring/models.py
+++ monitoring/models.py
@@ -1,40 +1,61 @@
 import uuid
 
 from django.db import models
+from django.utils import timezone
+
+
+class Group(models.Model):
+    """
+    Group Model: means of categorizing agents
+        Primary key is a slug-like, URL friendly and Human readable string called name which will be used to identify
+        each group.
+        Agents will use this identifier in their config files.
+
+    """
+    name = models.SlugField(verbose_name="Name", primary_key=True, max_length=64)
+
+    @property
+    def endpoints(self):
+        return Endpoint.objects.filter(monitoring_policy__groups=self)
+
+    def __str__(self):
+        return f"Group: {self.name}"
 
 
 class Agent(models.Model):
     """
-    Each Agent instance holds require information to access an agent
+    Agent Model:
+
+        Primary key is agent IP address, because this design considered agent IP address as it's identity.
+        Name is a human-readable name for agent to be used in reports
+        country is the country code according to ISO 3166-1 alpha-3 where agent's server is located.
+        last_activity is the datetime of the last time server received a valid request from this agent,
+        List of agents will be ordered by recent active agents by defaults
     """
-    name = models.SlugField(verbose_name="Agent ID", primary_key=True, help_text="Unique Human readable slug for agent")
-    namespace = models.CharField(max_length=64, help_text="Examples: europe.germany.hetzner")
-    url = models.URLField("Agent Access URL")
-    token = models.CharField(max_length=128, )
-    status = models.CharField(max_length=16, choices=[
-        ("ACTIVE", "Active Agent"),
-        ("INACTIVE", "Inactive Agent"),
-    ])
-    details = models.TextField(default="", help_text="last access information")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    ip = models.CharField(verbose_name="IP Address", max_length=15)
+    name = models.CharField(verbose_name="Agent name", max_length=64)
+    country = models.CharField(verbose_name="Country code", max_length=3)
+    groups = models.ManyToManyField(Group, related_name="agents")
+    last_activity = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f"Agent: {self.name} located in {self.namespace} status is {self.status}"
+        return f"Agent: {self.name} located at {self.country} IP={self.ip}"
 
     class Meta:
-        index_together = [
-            ("namespace",),
-            ("status",),
-            ("status", "namespace")
-        ]
+        ordering = ("-last_activity",)
 
 
 class Endpoint(models.Model):
+    """
+    Endpoint Model:
+
+        Primary key is an auto-generated UUID, UUID has been chosen to prevent accessing endpoints sequentially
+        name is a unique url-friendly human-readable string to be used in URLs instead of ugly UUIDs
+        List of endpoints will be ordered by their creation date by default
+
+    """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.SlugField(verbose_name="Endpoint name", db_index=True, help_text="Human readable slug for Endpoint")
-    description = models.TextField(default="")
-    active = models.BooleanField(default=True, db_index=True)
+    name = models.SlugField(verbose_name="Endpoint name", unique=True, help_text="Human readable slug for Endpoint")
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
 
     def __str__(self):
@@ -45,32 +66,44 @@ class Endpoint(models.Model):
 
 
 class HTTPEndpointDetail(models.Model):
+    """
+    HTTP details of Endpoint:
+        Endpoint model did not specified type of the service intentionally, details of the endpoint has been separated
+        in order to allow the design to support protocols other than HTTP in the future.
+        each HTTP endpoint will have a one to one relationship with an HTTPEndpointDetail.
+
+        hostname and port provide the address of the Endpoint
+        path, port and tls are details of HTTP Endpoints
+        maximum_expected_timeout indicates how much agent should wait for this endpoint to respond before it consider
+        endpoint problematic.
+
+    """
     endpoint = models.OneToOneField(Endpoint, on_delete=models.CASCADE, related_name="http_details")
     hostname = models.CharField(max_length=64, verbose_name="Hostname")
-    path = models.CharField(max_length=128, verbose_name="URL Path")
     port = models.CharField(max_length=5, verbose_name="Port")
+
+    path = models.CharField(max_length=128, verbose_name="URL Path")
     method_name = models.CharField(max_length=8, verbose_name="HTTP Method name", choices=[
         ("GET", "GET"),
         ("POST", "POST")
     ])
-
     tls = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    maximum_expected_timeout = models.IntegerField(default=30000)
 
     def __str__(self):
         return f"HTTP Endpoint Detail: {'https' if self.tls else 'http'}{self.hostname}:{self.port} at {self.path}"
 
 
 class MonitoringPolicy(models.Model):
+    """
+    Policy of monitoring: describe how system should monitor a specific Endpoint
+    When it comes to monitoring a service, there are some details that are not relevant to the service itself, but they
+    are required for monitoring.
+
+    """
     endpoint = models.OneToOneField(Endpoint, on_delete=models.CASCADE, related_name="monitoring_policy")
-    agent_selector = models.CharField(max_length=64, default="", blank=True,
-                                      help_text="Examples: europe.* or hetzner.* depending on how you "
-                                                               "defined your agents namespaces")
     interval = models.PositiveIntegerField(default=30, verbose_name="Interval in seconds")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    groups = models.ManyToManyField(Group, related_name="+")
 
     def __str__(self):
-        return f"Monitoring Policy: Monitoring {self.endpoint_id} every {self.interval} second(s) " \
-               f"using {self.agent_selector}"
+        return f"Monitoring Policy: Monitoring {self.endpoint_id} every {self.interval} second(s) "
--- scheduler/models.py
+++ scheduler/models.py
@@ -8,64 +8,67 @@ from monitoring import models as monitoring_models
 
 class Inspection(models.Model):
     """
-    a new object will created each time we start inspecting an endpoint by one or more agent
-    an Inspection could have multiple InspectionTask, depend on the number of agents
+    Inspection Model:
+    describe an specific inspection of an specific endpoint which happened (or will happen) in a specific time
+    Each Inspection could have zero, one or more corresponding HTTPInspectionResult objects.
+
+    Primary Key is a UUID to prevent sequentially traversing list of inspections.
+    timestamp is the point in time where inspection happened(or will be happen)
+
+    List of inspection will be ordered by timestamp by default.
+
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    timestamp = models.DateTimeField()
     endpoint = models.ForeignKey(monitoring_models.Endpoint, on_delete=models.CASCADE)
-    status = models.CharField(max_length=16, default="PENDING", choices=[
-        ("PENDING", "PENDING"),
-        ("FINISHED", "FINISHED")
-    ])
-    started_at = models.DateTimeField(auto_now_add=True)
-    finished_at = models.DateTimeField(null=True, default=None)
-    took = models.PositiveIntegerField(null=True, default=None)
 
-    def mark_as_finish(self, commit=True):
-        self.status = "FINISHED"
-        self.finished_at = timezone.now()
-        self.took = round((self.finished_at - self.started_at).total_seconds(), 0)
-        if commit:
-            self.save()
-
-
-class InspectionTask(models.Model):
-    """
-    a new object will created each time we ask an agent to inspect an endpoint
-    """
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    inspection = models.ForeignKey(Inspection, on_delete=models.CASCADE, related_name="tasks")
-    agent = models.ForeignKey(monitoring_models.Agent, on_delete=models.CASCADE)
-    status = models.CharField(max_length=16, default="PENDING", choices=[
-        ("PENDING", "PENDING"),
-        ("SUCCEED", "SUCCEED"),
-        ("FAILED", "FAILED"),
-    ])
-    error = models.TextField(default="")
-
-    started_at = models.DateTimeField(auto_now_add=True)
-    finished_at = models.DateTimeField(null=True, default=None)
-    took = models.PositiveIntegerField(null=True, default=None)
-
-    @property
-    def result(self):
-        return self.http_result
-
-    def mark_as_finish(self, error_message=None, commit=True):
-        self.status = "SUCCEED" if error_message is None else "FAILED"
-        self.error = error_message
-        self.finished_at = timezone.now()
-        self.took = round((self.finished_at - self.started_at).total_seconds(), 0)
-        if commit:
-            self.save()
+    class Meta:
+        ordering = ('timestamp', )
+        unique_together = [
+            ("endpoint", "timestamp")
+        ]
 
 
 class HTTPInspectionResult(models.Model):
+    """
+    HTTP Inspection Result Model:
+    Keep the result of an HTTP Inspection, it uses a UUID to prevent sequentially traversing list of results.
+
+    agent and agent_ip fields are responsible to keep reference to the agent who submit this specific result
+
+    connection_status field will be:
+        - SUCCEED: if the agent managed to received a valid HTTP response.
+        - TIMED-OUT: if the agent didn't received a valid HTTP response because of a timeout error.
+        - CONN-FAILED: if the agent didn't received a valid HTTP response for any reason other than timeout.
+    status_code field will contain the response status_code, if there was a response otherwise it will be null
+    response_time field will contain the whole time it took for agent to receive a response since it opened the socket,
+    if there was a successful connection.
+    byte_received field will contain the size of the response if there was a response otherwise it will be null .
+    submitted_at field will contain the time where server received the result from the agent
+    list of results will be sorted by result submit time by default
+
+    """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    inspection_task = models.OneToOneField(InspectionTask, on_delete=models.CASCADE, related_name="http_result")
-    successful_connection = models.BooleanField()
-    status_code = models.CharField(max_length=4)
-    dns_lookup_time = models.DecimalField(max_digits=6, decimal_places=3, null=True)
-    connect_time = models.DecimalField(max_digits=6, decimal_places=3, null=True)
+    inspection = models.ForeignKey(Inspection, on_delete=models.CASCADE, related_name="http_results")
+
+    agent = models.ForeignKey(monitoring_models.Agent, on_delete=models.SET_NULL, null=True)
+    agent_ip = models.CharField(max_length=32)
+
+    # Result:
+    connection_status = models.CharField(max_length=16, choices=[
+        ("SUCCEED", "SUCCEED", ),
+        ("CONN-FAILED", "CONN-FAILED", ),
+        ("TIMED-OUT", "TIMED-OUT", )
+    ])
+    status_code = models.CharField(max_length=4, null=True)
     response_time = models.DecimalField(max_digits=6, decimal_places=3, null=True)
     byte_received = models.PositiveIntegerField(null=True)
+
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = [
+            ("inspection", "agent_ip")
+        ]
+        ordering = ("submitted_at", )
