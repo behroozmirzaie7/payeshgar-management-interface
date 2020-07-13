da5d00f9e7992431abed5eb6f7047ac81770ffa3
Mahdi Zareie <mahdi.zareie@pm.me>
2020-07-14 02:31:27 +0430

    Configure celery beat to call schedule inspection generation

--- payeshgar_server/celery.py
+++ payeshgar_server/celery.py
@@ -17,9 +17,24 @@ app.conf.task_queues = (
         Exchange('new_result'),
         routing_key='new_result',
         queue_arguments={'x-max-priority': 1}
-    )
+    ),
+    Queue(
+        'inspection_generation',
+        Exchange('inspection_generation'),
+        routing_key='inspection_generation',
+        queue_arguments={'x-max-priority': 10}
+    ),
 )
 
 app.conf.task_routes = {
     'inspecting.tasks.process_results': {'queue': 'new_result'},
+    'inspecting.tasks.generate_inspections': {'queue': 'inspection_generation'},
+}
+
+app.conf.beat_schedule = {
+    'generate_inspections': {
+        'task': 'inspecting.tasks.generate_inspections',
+        'schedule': 300.0,
+    },
+
 }
